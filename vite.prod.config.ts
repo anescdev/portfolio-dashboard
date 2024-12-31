29be2c10765c967cb9dc2d1a5e7b9eee059544ca
anescdev <anescdev@gmail.com>
2024-12-31 16:52:43 +0100

    Changed basename

--- vite.prod.config.ts
+++ vite.prod.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dashboard/",
+  base: "/dashboard",
   plugins: [react()],
   resolve: {
     alias: {
